6d744cb790838fadf2a21c663dee8af9f2f0f187
Chelovecow <127473860+Chelovecow@users.noreply.github.com>
2023-03-10 11:05:33 +0300

    Add files via upload

---
+++ Лабароторная работа/lab_1.py
@@ -0,0 +1,25 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+
+@app.route("/silka/") 
+def page_silka(): 
+    return "<a href='www.google.com'>Ссыка</a>"
+
+
+@app.route('/haha//<int:chislo>')
+def haha(chislo):
+    print(chislo)
+    print(int(chislo))
+    return f'<h1>Вы написали число {chislo} в URL строке</hl>'
+
+
+
+
+
+
+
+app.run(host='127.0.0.21', port=5000)
